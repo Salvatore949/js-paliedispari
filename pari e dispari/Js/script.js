f04ca5f82d6a0504d3b76cc514c069079692469a
Salvatore949 <salvo.mess49@gmail.com>
2021-11-02 13:37:12 +0100

    struttura

---
+++ pari e dispari/Js/script.js
@@ -0,0 +1,6 @@
+//L'utente iserisce un numero da 1 a 5
+//attraverso Math.floor genero un numero random
+//Sommo i numeri( sommo le variabili assegnato al numero inserito dall'utente e quello generato dal pc)
+//Creo le condizioni con if ed else. Se la somma, alla quale assegno un valore è ===0 allora l'utente ha vinto altrimenti il il valore è ===1 allora l'utente ha perso
+
+var domanda = parseInt(prompt("Inserisci un numero da 1 a 5"));
